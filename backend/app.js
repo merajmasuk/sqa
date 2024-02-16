3872f23b8e74c7bbf21e7e80296bd85e8dde119e
jakir hossain <jucse28.382@gmail.com>
2024-02-16 23:39:15 +0600

    added file

--- backend/app.js
+++ backend/app.js
@@ -88,6 +88,10 @@ app.use((req, res, next) => {
     res.status(404).json({ message: "Route not found" });
 });
 
+
+
+
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: "Internal Server Error" });
